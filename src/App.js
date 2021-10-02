8736e2bf80f400d9c3f3c7c067c09237bdc0bc7f
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-02 22:43:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
-import { Component1 } from "./components/Component1";
-import { Component2} from "./components/Component2";
-import { Component3} from "./components/Component3";
-import { Component4} from "./components/Component4";
-import { Component5} from "./components/Component5";
-import { Component6} from "./components/Component6";
-import { Component7} from "./components/Component7";
-import { Component8} from "./components/Component8";
-import { Component9} from "./components/Component9";
-import { Component10} from "./components/Component10";
+// import { Component1 } from "./components/Component1";
+// import { Component2} from "./components/Component2";
+// import { Component3} from "./components/Component3";
+// import { Component4} from "./components/Component4";
+// import { Component5} from "./components/Component5";
+// import { Component6} from "./components/Component6";
+// import { Component7} from "./components/Component7";
+// import { Component8} from "./components/Component8";
+// import { Component9} from "./components/Component9";
+// import { Component10} from "./components/Component10";
 function App() {
+  const mystyle={
+    color:"blue",
+  };
   return (
-    <div className="App">
-      <Component1/>
-      <Component2/>
-      <Component3/>
-      <Component4/>
-      <Component5/>
-      <Component6/>
-      <Component7/>
-      <Component8/>
-      <Component9/>
-      <Component10/>
-    </div> 
+    // <div className="App">
+    //   <Component1/>
+    //   <Component2/>
+    //   <Component3/>
+    //   <Component4/>
+    //   <Component5/>
+    //   <Component6/>
+    //   <Component7/>
+    //   <Component8/>
+    //   <Component9/>
+    //   <Component10/>
+    // </div> 
+
+  <div className="App">
+    <h1 style={{ color:"yellow", }}>Inline Style</h1>
+    <h1 style={mystyle}>Internal Style</h1>
+  </div>
   );
 };
 
