8df295e7f74686c3177c236bebf8d2e97a4b0a91
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-18 15:02:10 +0530

    new-prac

--- src/components/ClassComponent/ClassCounter.jsx
+++ src/components/ClassComponent/ClassCounter.jsx
@@ -1,31 +1,30 @@
 import React, { Component} from "react";
+import { ClassCountChild } from "./ClassCountChild";
  class ClassCount extends Component{
   constructor(props){
       super(props);
       this.state={count:0};
   }
-//   handleIncrement=()=>{
-//         this.setState((prevState)=>({count:prevState.count+1}))
-//   };
-// //   handleDecrement=()=>{
-//       this.setState((prevState)=>({count:prevState.count-1}))
-//   };
-//   handleReset=()=>{
-//       this.setState((prevState)=>({count:prevState.count*0}))
-//   }
+  handleIncrement=()=>{
+        this.setState((prevState)=>({count:prevState.count+1}))
+  };
+  handleDecrement=()=>{
+      this.setState((prevState)=>({count:prevState.count-1}))
+  };
+  handleReset=()=>{
+      this.setState((prevState)=>({count:prevState.count*0}))
+  }
  
   render(){
       return(
           <div>
-              <h1> Class Component Counter</h1>
-              <h4>Count:{this.state.count}
-              </h4>
-            <button onClick={()=> this.setState((prevState)=>({count:prevState.count+1}))} disabled={this.state.count<=10}>increament</button>
-            <button onClick={()=> this.setState((prevState)=>({count:prevState.count-1}))} disabled={this.state.count >  0}>Decreament</button>
-            <button onClick={()=> this.setState((prevState)=>({count:prevState.count*0}))}>Reset</button>
+            <ClassCountChild showCounter={this.state.count} CountIncrement={this.handleIncrement} CountDecrement={this.handleDecrement} CountReset={this.handleReset} />
+            {/* <button onClick={()=> this.setState((prevState)=>({count:prevState.count+1}))}>increament</button>
+            <button onClick={()=> this.setState((prevState)=>({count:prevState.count-1}))} >Decreament</button>
+            <button onClick={()=> this.setState((prevState)=>({count:prevState.count*0}))}>Reset</button> */}
 
           </div>
       )
-  }
- }
+  };
+ };
  export default ClassCount
